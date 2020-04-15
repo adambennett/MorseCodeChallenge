35cc16e8385fe5358230382442227f87eaa38ab1
Adam Bennett <nyoxidetwitter@gmail.com>
2020-04-14 22:03:09 -0400

    all tests passing

--- src/main/java/Decoder.java
+++ src/main/java/Decoder.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.logging.*;
 
 public class Decoder {
 
@@ -14,11 +13,8 @@ public class Decoder {
         char indicated = encoded.charAt(position);
         int output = 0;
         for (int i = position; i < encoded.length(); i++) {
-            if (encoded.charAt(i) == indicated) {
-                output++;
-            } else {
-                break;
-            }
+            if (encoded.charAt(i) == indicated) { output++;}
+            else { break; }
         }
         return output;
     }
@@ -39,8 +35,6 @@ public class Decoder {
             else if (note.toString().equals(dih)) { return MorseNotation.SKIP; }
             else if (note.toString().equals(spacer) || note.toString().equals(altSpacer)) { return MorseNotation.SPACE; }
         }
-
-        Logger.getGlobal().warning("getNote() didn't find something to return!");
         return MorseNotation.SPACE;
     }
 
@@ -50,9 +44,8 @@ public class Decoder {
         String curr = "" + current;
         int iter = 1;
         while (iter < encoded.length()) {
-            if (encoded.charAt(iter) == current) {
-                curr += encoded.charAt(iter);
-            } else {
+            if (encoded.charAt(iter) == current) { curr += encoded.charAt(iter); }
+            else {
                 magicStrings.add(curr);
                 current = encoded.charAt(iter);
                 curr = "" + current;
@@ -67,9 +60,7 @@ public class Decoder {
         int lengthOfShortest = -1;
         for (String s : strings) {
             if (lengthOfShortest == -1) { lengthOfShortest = s.length(); }
-            else if (s.length() < lengthOfShortest) {
-                lengthOfShortest = s.length();
-            }
+            else if (s.length() < lengthOfShortest) { lengthOfShortest = s.length(); }
         }
         return lengthOfShortest;
     }
@@ -96,10 +87,42 @@ public class Decoder {
         for (int i = 0; i < 7; i++) { altSpacer += altDih; }
     }
 
+    public static String recode(String encoded, TreeMap<Character, Integer> freq) {
+        if (freq.keySet().size() > 2) {
+            ArrayList<Character> goodGuys = new ArrayList<>();
+            for (Map.Entry<Character, Integer> entry: freq.entrySet()) {
+                if (goodGuys.size() < 2) { goodGuys.add(entry.getKey()); }
+                else { break; }
+            }
+            String newEncoded = "";
+            for (int i = 0; i < encoded.length(); i++) {
+                if (goodGuys.contains(encoded.charAt(i))) { newEncoded += encoded.charAt(i); }
+                else {
+                    for (int j = i+1; j < encoded.length(); j++) {
+                        if (goodGuys.contains(encoded.charAt(j))) {
+                            newEncoded += encoded.charAt(j);
+                            break;
+                        }
+                    }
+                }
+            }
+           return newEncoded;
+        }
+        return encoded;
+    }
+
+    public static String getNewEncoding(String encoding) {
+        TreeMap<Character, Integer> freq = new TreeMap<>();
+        for (char c : encoding.toCharArray()) {
+            if (!Character.isAlphabetic(c) && !Character.isDigit(c)) { throw new IllegalArgumentException(); }
+            else { freq.compute(c, (k, v) -> (v==null) ? 1 : v+1); }
+        }
+        return recode(encoding, freq);
+    }
+
     public static String decode (String encoded) {
-        for (char c : encoded.toCharArray()) { if (!Character.isAlphabetic(c) && !Character.isDigit(c)) { throw new IllegalArgumentException(); }}
+        encoded = getNewEncoding(encoded);
         Set<String> magicStrings = getMagics(encoded);
-        //if (magicStrings.size() > 5) { throw new RuntimeException("aint got the brain for all this yet..."); }
         Integer lengthOfShortest = getShortestLength(magicStrings);
         setDihs(magicStrings, lengthOfShortest);
         setOtherVals();
@@ -114,7 +137,6 @@ public class Decoder {
             if (nextNote.equals(MorseNotation.DIH) || nextNote.equals(MorseNotation.DAH)) {
                 ticks.add(nextNote);
             } else if (nextNote.equals(MorseNotation.DONE)) {
-                MorseCharacter test = new MorseCharacter(ticks, null);
                 Optional<MorseCharacter> toAdd = MorseCharactersArchive.getInstance().getCharacter(ticks);
                 if (toAdd.isPresent()) {
                     phrase.add(toAdd.get());
@@ -131,9 +153,7 @@ public class Decoder {
             } else if (nextNote.equals(MorseNotation.SPACE)) {
                 Optional<MorseCharacter> finalChar = MorseCharactersArchive.getInstance().getCharacter(ticks);
                 finalChar.ifPresent(phrase::add);
-                if (phrase.size() > 0) {
-                    phrase.add(new MorseCharacter());
-                }
+                if (phrase.size() > 0) { phrase.add(new MorseCharacter()); }
                 ticks.clear();
             }
             iter += hm;
@@ -142,5 +162,7 @@ public class Decoder {
         finalChar.ifPresent(phrase::add);
         return new MorsePhrase(phrase).getOutput().trim();
     }
+
+
 
 }
--- src/main/java/MorseNotation.java
+++ src/main/java/MorseNotation.java
@@ -1,3 +1,3 @@
 public enum MorseNotation {
-    DIH,DAH, SPACE, SKIP, DONE
+    DIH, DAH, SPACE, SKIP, DONE
 }
